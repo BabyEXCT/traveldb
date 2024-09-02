f2acc28483b420fd175a943bc6d6ebebc7d57d4e
BabyEXCT <61609618+BabyEXCT@users.noreply.github.com>
2024-09-02 09:05:06 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  output: 'standalone',
+  images: {
+    domains: ['traveldb-git-main-babyexcts-projects.vercel.app'], // Add your domain here for Next.js Image component support
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
